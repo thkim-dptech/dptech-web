e5d74f964162aa7e5cc3c14445dc00f48e471ed6
thkim-dptech <travel4me@naver.com>
2026-03-13 12:33:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,43 @@
-const CACHE_NAME = 'dptech-v1';
+const CACHE_NAME = 'dptech-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-// 서비스 워커 설치 시 캐시 저장
+// 서비스 워커 설치: 필수 리소스 캐싱
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/', '/index.html']);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
   self.skipWaiting();
 });
 
-// 네트워크 요청 시 응답 처리 (설치 조건 충족용)
+// 새로운 서비스 워커 활성화 시 구버전 캐시 삭제
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 안드로이드 크롬의 '설치 가능' 판단 기준인 fetch 이벤트 리스너
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then((response) => {
+        // 캐시에 있으면 반환, 없으면 네트워크에서 가져옴
+        return response || fetch(event.request);
+      })
   );
 });
